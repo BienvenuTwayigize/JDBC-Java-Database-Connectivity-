338d2e6f98214f162453d8bf58fff5901e4d49a3
Twayigize Bienvenu <119926550+BienvenuTwayigize@users.noreply.github.com>
2023-04-08 16:41:32 -0700

    Java Database Connectivity

---
+++ javasql/JavaSQL.java
@@ -0,0 +1,12 @@
+package javasql;
+
+public class JavaSQL {
+
+    public static void main(String[] args) {
+       
+    
+    DBconnection obj= new DBO();
+    obj.selectOperation(1818 );
+  
+    }
+}
